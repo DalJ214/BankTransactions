0bc0d04194556543fa26363727061ea8e294f683
DalJ214 <joefazluna@gmail.com>
2024-10-17 17:06:42 -0500

    now saving to the csv in the proper format

--- FinancialTransactions/src/main/java/com/pluralsight/Main.java
+++ FinancialTransactions/src/main/java/com/pluralsight/Main.java
@@ -1,19 +1,28 @@
 package com.pluralsight;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
+
 
         try {
 
 
             Scanner Scan = new Scanner(System.in);
             boolean run = true;
-            FileWriter fileWriter = new FileWriter("transactions.csv"); //To write to file
+            FileWriter fileWriter = new FileWriter("src/main/resources/transactions.csv");
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             List<Transaction> transactions = new ArrayList<>(); // Array to store transactions
+
+
 
             while (true) {  //Home screen menu that require user input
                 System.out.println("    Home Screen   ");
@@ -26,41 +35,105 @@ public class Main {
 
                 switch (selection) {
                     case "1":
-                        System.out.println("Adding Deposit ");
-                        System.out.println("Enter Deposit details " );
-                        String input = Scan.nextLine();
+                        //get date and time
+                        LocalDateTime localDateTime = LocalDateTime.now();
+                        LocalDate date = localDateTime.toLocalDate();
+                        LocalTime time = localDateTime.toLocalTime();
+                        //what the user needs to input
+                        System.out.print("Enter Description: ");
+                        String description = Scan.nextLine();
+                        System.out.print("Enter vendor: ");
+                        String vendor = Scan.nextLine();
+                        System.out.print("Enter Deposit amount:");
+                        double depositAmount = Scan.nextDouble();
+                        Scan.nextLine();
+                        if (depositAmount > 0) {
+                            //formatting the transaction as a csv string
+                            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-                        if (processTransaction(input, transactions, bufferedWriter)){
-                            System.out.println("Deposited successfully");
-                        }else{
-                            System.out.println("Invalid input, Please try again");
+                            String transactionCSV = String.format("%s|%s|%s|%s|%.2f",
+                            date.format(dateFormatter),
+                            time.format(timeFormatter),
+                                    description,
+                                    vendor,
+                                    depositAmount);
+                            //write to the csvFile
+                            bufferedWriter.write(transactionCSV);
+                            bufferedWriter.newLine();
+                            bufferedWriter.flush(); // ensuring data is written to file
+                            System.out.println("Deposited successfully. ");
+
+
+
                         }
+                        System.out.println("Press enter to exit. ");
+                        Scan.nextLine();
                         break;
                     case "2":
-                        System.out.println("Making a payment  ");
-                        System.out.println("Enter transaction details ");
-                        input = Scan.nextLine();
+                        LocalDateTime localDateTime1 = LocalDateTime.now();
+                        LocalDate date1 = localDateTime1.toLocalDate();
+                        LocalTime time1 = localDateTime1.toLocalTime();
 
-                        
-                        // code block
+                        System.out.print("Enter Description: ");
+                        String newdescription = Scan.nextLine();
+                        System.out.print("Enter vendor: ");
+                        String newvendor = Scan.nextLine();
+                        System.out.print("Enter payment amount : ");
+                        double paymentAmount = Scan.nextDouble();
+                        Scan.nextLine();
+                        if (paymentAmount > 0) {
+                            DateTimeFormatter dateFormatter1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                            DateTimeFormatter timeFormatter1 = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+                            String transactionCSV = String.format("%s|%s|%s|%s|%.2f",
+                                    date1.format(dateFormatter1),
+                                    time1.format(timeFormatter1),
+                                    newdescription,
+                                    newvendor,
+                                    paymentAmount *= -1 );
+                            //wrties to a negative amount
+                            //write to the csvFile
+                            bufferedWriter.write(transactionCSV);
+                            bufferedWriter.newLine();
+                            bufferedWriter.flush();
+
+                            System.out.println("Payment made successfully. ");
+                        }
+                        System.out.println("Press enter to exit. ");
+                        Scan.nextLine();
                         break;
                     case "3":
                         System.out.println("Viewing Ledger ");
+                        for (Transaction t : transactions) {
+                            System.out.println(t);
+                        }
+                        System.out.println("Press enter to exit. ");
+                        Scan.nextLine();
                         break;
                     case "4":
-                        System.out.println("Exit ");
-                        run = false;
-                        break;
+                        System.out.println("Goodbye ");
+                        bufferedWriter.close();
+                        Scan.close();
+                        return;
                     default:
                         System.out.println("Invalid choice. Please enter a valid option.");
-                        // code block
+
+
                 }
 
             }
 
 
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+
     }
 }
+
+
+
+
+
--- FinancialTransactions/src/main/java/com/pluralsight/Transaction.java
+++ FinancialTransactions/src/main/java/com/pluralsight/Transaction.java
@@ -7,11 +7,24 @@ public class Transaction {
     private String vendor;
     private double amount;
 
-    public Transaction(String date, String time , String description, String vendor, double amount){
-        this.date = date;
-        this.time = time;
-        this.description = description;
-        this.vendor = vendor;
-        this.amount = amount;
+    public Transaction(){
+        this.date = this.date;
+        this.time = this.time;
+        this.description = this.description;
+        this.vendor = this.vendor;
+        this.amount = this.amount;
+    }
+
+    public String getDate(){
+        return date;
+    }
+    public String getTime(){
+        return time;
+    }
+    public String getVendor(){
+        return vendor;
+    }
+    public double getAmount(){
+        return amount;
     }
 }
